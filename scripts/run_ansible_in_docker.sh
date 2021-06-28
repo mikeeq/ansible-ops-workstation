bdae04fc48ab79fef6957b774ca0d1b5df2fa51d
mikeeq <miotk.mikolaj@gmail.com>
2021-06-28 08:38:59 +0200

    Revert roles'

--- scripts/run_ansible_in_docker.sh
+++ scripts/run_ansible_in_docker.sh
@@ -16,4 +16,13 @@ docker exec \
   fedora_systemd /bin/bash -c " \
     ansible-playbook -e docker_tests_flag=true -e ansible_python_interpreter=/usr/bin/python3 -i inventory/hosts.yml fedora.yml"
 
+docker exec \
+  -t \
+  fedora_systemd /bin/bash -c " \
+    ansible-playbook -e docker_tests_flag=true -e ansible_python_interpreter=/usr/bin/python3 -i inventory/hosts.yml fedora.yml"
+
+livecd_exitcode=$?
+
 docker stop fedora_systemd
+
+exit $livecd_exitcode
