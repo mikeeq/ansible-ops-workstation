614fec9a1b64c27df887da3fa42c64c17e312ee5
Mikołaj Miotk <miotk.mikolaj@gmail.com>
2021-10-18 14:34:24 +0200

    Add WSL ubuntu provisioning (#3)

--- scripts/run_ansible_in_docker.sh
+++ scripts/run_ansible_in_docker.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+DOCKER_IMAGE=${DOCKER_IMAGE:-fedora_systemd}
+ANSIBLE_PLAYBOOK=${ANSIBLE_PLAYBOOK:-fedora.yml}
 docker run \
-  --name fedora_systemd \
+  --name ${DOCKER_IMAGE} \
   -d \
   -t \
   --privileged \
@@ -9,20 +11,20 @@ docker run \
   -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
   -w /repo \
   --rm \
-  fedora-systemd:latest
+  ${DOCKER_IMAGE}
 
 docker exec \
   -t \
-  fedora_systemd /bin/bash -c " \
-    ansible-playbook -e docker_tests_flag=true -e ansible_python_interpreter=/usr/bin/python3 -i inventory/hosts.yml fedora.yml"
+  ${DOCKER_IMAGE} /bin/bash -c " \
+    ansible-playbook -e docker_tests_flag=true -e ansible_python_interpreter=/usr/bin/python3 -i inventory/hosts.yml ${ANSIBLE_PLAYBOOK}"
 
 docker exec \
   -t \
-  fedora_systemd /bin/bash -c " \
-    ansible-playbook -e docker_tests_flag=true -e ansible_python_interpreter=/usr/bin/python3 -i inventory/hosts.yml fedora.yml"
+  ${DOCKER_IMAGE} /bin/bash -c " \
+    ansible-playbook -e docker_tests_flag=true -e ansible_python_interpreter=/usr/bin/python3 -i inventory/hosts.yml ${ANSIBLE_PLAYBOOK}"
 
 livecd_exitcode=$?
 
-docker stop fedora_systemd
+docker stop ${DOCKER_IMAGE}
 
 exit $livecd_exitcode
