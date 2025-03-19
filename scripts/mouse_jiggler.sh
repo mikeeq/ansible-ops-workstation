2226064da48bd8c6ba15145a2734c2aefe1b216a
mikeeq <miotk.mikolaj@gmail.com>
2025-03-19 14:29:32 +0100

    fix wayland

--- scripts/mouse_jiggler.sh
+++ scripts/mouse_jiggler.sh
@@ -22,12 +22,24 @@ set -x
 
 # x:3864 y:581
 # Define mouse coordinates for the left click (example: x=500, y=300)
-MOUSE_X=3864
-MOUSE_Y=581
+MOUSE_X=3872
+MOUSE_Y_1=790
+MOUSE_Y_2=842
+
+get_idle_time_wayland() {
+    dbus-send --print-reply --dest=org.gnome.Mutter.IdleMonitor \
+        /org/gnome/Mutter/IdleMonitor/Core \
+        org.gnome.Mutter.IdleMonitor.GetIdletime \
+        | awk '/uint64/ {print $2}'
+}
 
 # Function to detect if the cursor has moved in the last 5 minutes
 check_cursor_idle() {
-    idle_time=$(xprintidle)  # Requires xprintidle installed, returns idle time in milliseconds
+    if [ $XDG_SESSION_TYPE == "wayland" ]; then
+        idle_time=$(get_idle_time_wayland)  # Requires xprintidle installed, returns idle time in milliseconds
+    else
+        idle_time=$(xprintidle)
+    fi
     if [[ "$idle_time" -ge 300 ]]; then  # 5 minutes = 300000 milliseconds
         return 0  # Idle for 5 minutes
     else
@@ -51,8 +63,10 @@ while true; do
             sleep 1
 
             # Left click at the specified coordinates
-            xdotool mousemove $MOUSE_X $MOUSE_Y click 1
-            xdotool mousemove $MOUSE_X 511 click 1
+            xdotool mousemove $MOUSE_X $MOUSE_Y_1 click 1
+            xdotool mousemove $MOUSE_X $MOUSE_Y_2 click 1
+            # ydotool
+            # dotool
 
             # Wait for 1 second before switching to the next screen
             sleep 1
@@ -60,13 +74,15 @@ while true; do
             # Switch to screen 3
             # xdotool key --window $(xdotool getactivewindow) F2  # Example for switching to screen 3
             xdotool set_desktop 3
+            # wmctrl -s 3
 
             # Wait for a moment
             sleep 1
 
             # Left click at the specified coordinates
-            xdotool mousemove $MOUSE_X $MOUSE_Y click 1
-            xdotool mousemove $MOUSE_X 511 click 1
+            xdotool mousemove $MOUSE_X $MOUSE_Y_1 click 1
+            sleep 2
+            xdotool mousemove $MOUSE_X $MOUSE_Y_2 click 1
 
             # Check every 10 seconds if the cursor is still idle
             sleep 10
